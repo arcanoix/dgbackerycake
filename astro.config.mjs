4942db5af12c58b593fad79cc63328fb4e55ddfb
Gustavo Herrera <gustavoh.2312@gmail.com>
2025-03-10 14:51:38 -0400

    add google analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://www.dgbakerycake.com',
+    integrations: [
+        mdx(), 
+        sitemap(),
+        partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),
+    ],
     vite: {
         server: {
             watch: { usePolling: true }
